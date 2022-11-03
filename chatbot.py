63de569e1c3d67d336e638030773a7d45f276628
iKollor <isai_aulayvida@hotmail.com>
2022-11-03 00:41:32 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -13,14 +13,15 @@ mic = sr.Microphone(device_index=1)
 # input
 user_name = input("Escribe tu nombre: ")
 ia_name = input("Escribe el nombre de la IA: ")
-context = ia_name + \
-    " es una asistente virtual muy sarcástica y bromista aun asi servicial a "+user_name
+context = ia_name + " es una asistente virtual muy sarcástica y bromista aun asi servicial a "+user_name
 #input("Describe a tu IA: ")
 
 # start
 engine = pyttsx3.init()
 listener = sr.Recognizer()
 text = ""
+conversation = ""
+
 isListening = False
 
 # sr properties
@@ -33,25 +34,22 @@ engine.setProperty('voice', voices[2].id)
 #    print (voice, voice.id)
 
 
-def gpt3(text):
+def gpt3(text, conversation):
     # using GPT3
-    conversation = ""
+    prompt = user_name + ": " + text + "\n"+ia_name+": "
     print(user_name+": "+text)
-    conversation += "\n"+user_name+": " + text + "\n"+ia_name+":"
+    conversation += prompt
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=(context+"\n\n"+conversation),
-        temperature=0.9,
+        temperature=0.9, 
         max_tokens=150,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
-        stop=["\n", " "+user_name+":", " "+ia_name+":"]
     )
-    response_str = response["choices"][0]["text"].replace("\n", "")
-    response_str = response_str.split(
-        user_name + ": ", 1)[0].split(ia_name + ": ", 1)[0]
-    conversation += response_str + "\n"
+    response_str = response["choices"][0]["text"].replace(ia_name+":", "").strip()
+    conversation += response_str
     print(ia_name+": "+response_str)
     return response_str
 
@@ -62,39 +60,44 @@ def talk(text):
 
 
 def listen():
-    with mic as source:
-        listener.adjust_for_ambient_noise(source, duration=0.2)
-        voice = listener.listen(source)
 
-    if isListening == False:
-        print("(("+ia_name+" no está escuchando))")
-    else:
+    if isListening == True:
         print("(("+ia_name+" está escuchando))")
+    else:
+        print("(("+ia_name+" no está escuchando))")
+
+    with mic as source:
+        listener.adjust_for_ambient_noise(source, duration=0.3)
+        voice = listener.listen(source)
+        print("((Hablaste))")
 
     try:
         rec = listener.recognize_google(voice, language="es-ES")
-    except LookupError:
-        print(ia_name+": No te entendí\n")
-        engine.say(ia_name+": No te entendí")
-    except Exception:
-        print("((Esperando))")
-        return
+    except:
+        rec = "*"+user_name+" dice algo inentendible*"
     return rec
 
 
-print("(("+ia_name+" está escuchando))")
+print("((Di algo))")
+
 while True:
     text = listen()
     if ia_name in text:
         isListening = True
-        print("(("+ia_name+" está escuchando))")
-        while True:
-            response_str = gpt3(text)
+        while ("adiós" or "adios") not in text.lower():
+
+            response_str = gpt3(text, conversation)
+
+            if response_str == " ":
+                print(ia_name+": ¿Puedes repetirme por favor?")
+                talk("¿Puedes repetirme por favor?")
+            
             talk(response_str)
             text = listen()
-            if ("adiós" or "Adios" or "Adiós" or "adios") in text:
-                response_str = gpt3(text)
-                talk(response_str)
-                print("(("+ia_name+" se fue a dormir))")
-                isListening = False
-                break
+
+        response_str = gpt3(text, conversation)
+        talk(response_str)
+        print("(("+ia_name+" se fue a dormir))")
+        isListening = False
+    else:
+        continue
